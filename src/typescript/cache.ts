ba8c2dff109c6e3235c78cf043dcc41070d0cf08
sourcebits-robertbiggs <robert.biggs@sourcebits.com>
2015-08-02 11:11:38 -0700

    Conversion to TypeScript.
    The source files are now in TypeScript. After building, this is
    converted to JavaScript, same as before.
    Replaced Ajax methods with Fetch API from WHATWG.
    Added tests for Fetch API, requiring PHP server to run.

---
+++ src/typescript/cache.ts
@@ -0,0 +1,89 @@
+/// <reference path='../typings/tsd.d.ts' />
+/// <reference path='core.ts' />
+(function($){
+  $.extend({
+    chch_cache : {
+      data: {},
+      events: {
+        keys : <any>[],
+
+        values : <any>[],
+
+        set : function ( element: any, event: any, callback?: Function, capturePhase?: string ) {
+         var key;
+         var length = this.values.length > 0 ? this.values.length - 1 : 0;
+         var values;
+         if (!!element.id) {
+           key = element.id;
+         } else {
+           ++chocolatechipjs['uuid'];
+           key = chocolatechipjs['makeUuid']();
+           element.setAttribute("id", key);
+         }
+         if (this.keys.indexOf(key) >= 0) {
+           this.values[length].push([]);
+           values = this.values[length];
+           values.push(event);
+           values.push(callback);
+           values.push(capturePhase);
+           element.addEventListener(event,callback,capturePhase);
+         } else {
+           this.keys.push(key);
+           this.values.push([]);
+           length = this.values.length-1;
+           this.values[length].push([]);
+           values = this.values[length];
+           values[0].push(event);
+           values[0].push(callback);
+           values[0].push(capturePhase);
+           element.addEventListener(event,callback,capturePhase);
+         }
+        },
+
+
+        hasKey : function ( key: string ): boolean {
+         if (this.keys.indexOf(key) >= 0) {
+           return true;
+         } else {
+           return false;
+         }
+        },
+
+        _delete : function ( element, event, callback  ): any {
+         var $this = this;
+         var idx = this.keys.indexOf(element);
+         var cache = this.values;
+         if (!element) {
+           return;
+         }
+         if (typeof event === 'undefined') {
+           cache[idx].each(function(item) {
+             document.getElementById(element).removeEventListener(item[0], item[1], item[2]);
+             $['chch_cache'].events.keys.splice(idx, 1);
+             cache[idx].splice(idx, 1);
+           });
+           cache.splice(idx, 1);
+         }
+         if (event && callback) {
+           cache[idx].each(function(item) {
+             if (item[0] === event) {
+               document.getElementById(element).removeEventListener(item[0], item[1], item[2]);
+               $['chch_cache'].events.values.splice(idx, 1);
+               $['chch_cache'].events.keys.splice(idx, 1);
+             }
+           });
+         }
+         if (event && typeof callback === 'undefined') {
+           $this.values[idx].each(function(item) {
+             if (item[0] === event) {
+               document.getElementById(element).removeEventListener(item[0], item[1], item[2]);
+               $['chch_cache'].events.values.splice(idx, 1);
+               $['chch_cache'].events.keys.splice(idx, 1);
+             }
+           });
+         }
+        }
+      }
+    }
+  });
+})(chocolatechipjs);
---
+++ src/typescript/pubsub.ts
@@ -0,0 +1,50 @@
+/// <reference path="../typings/chocolatechipjs/chocolatechipjs.d.ts"/> />
+/// <reference path='core.ts' />
+(function($){
+  $.extend({
+
+    subscriptions : {},
+
+    subscribe : function ( topic: string, callback: Function ) {
+      var token = ($['uuidNum']());
+      if (!$['subscriptions'][topic]) {
+        $['subscriptions'][topic] = [];
+      }
+      $['subscriptions'][topic].push({
+        token: token,
+        callback: callback
+      });
+      return token;
+    },
+
+    publish : function ( topic: string, args: any ) {
+      if (!$['subscriptions'][topic]) {
+        return false;
+      }
+      setTimeout(function () {
+        var len = $['subscriptions'][topic] ? $['subscriptions'][topic].length : 0;
+        while (len--) {
+           $['subscriptions'][topic][len].callback(topic, args);
+        }
+        return true;
+      });
+    },
+
+    unsubscribe : function ( token: any ) {
+      setTimeout(function() {
+        for (var m in $['subscriptions']) {
+          if ($['subscriptions'][m]) {
+             for (var i = 0, len = $['subscriptions'][m].length; i < len; i++) {
+                if ($['subscriptions'][m][i].token === token) {
+                  $['subscriptions'][m].splice(i, 1);
+                  return token;
+                }
+             }
+          }
+        }
+        return false;
+      });
+    }
+
+  });
+})(chocolatechipjs);
